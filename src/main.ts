0cf43cf9059638c553ab0f099b980b1b628b2cc4
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-08-05 11:50:17 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
