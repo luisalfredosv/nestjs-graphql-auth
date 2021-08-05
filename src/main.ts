cdc09c6ffa1696013989f837fb41384415c195f4
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-08-05 16:34:49 -0400

    Use helmet and compression main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as compression from 'compression';
+
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
@@ -8,13 +12,19 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
+  // app.use(csurf());
+  app.use(compression());
+
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app)
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000
 
-  logger.log(`[Application listening] on port 3000`);
+  await app.listen(PORT);
+
+  logger.log(`[Application listening] on port ${PORT}`);
 }
 bootstrap();
