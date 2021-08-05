9a81f52f7de45275aead347f7da34e146c94e4f1
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-08-05 16:33:48 -0400

    Update local.strategies.ts

--- src/auth/strategies/local.strategies.ts
+++ src/auth/strategies/local.strategies.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 
@@ -9,15 +9,19 @@ import { AuthService } from '../auth.service';
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
-      usernameField: 'email',
+      idField: 'id',
     });
   }
 
   async validate(username: string, password: string): Promise<UserType> {
+   try {
     const user = await this.authService.validate({ email: username , password });
     if (!user) {
-      throw new UnauthorizedException('Local');
+      throw new UnauthorizedException('Unauthorized');
     }
     return user;
+   } catch (error) {
+     throw new InternalServerErrorException('x5559494')
+   }
   }
 }
