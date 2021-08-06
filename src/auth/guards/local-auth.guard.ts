ac3ef1ddebc41d35d9732e39d07aad0f9d034164
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-08-06 13:26:45 -0400

    Update jwt.auth.guard.ts and local-auth.guard.ts

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,2 +1,13 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
-export class LocalStrategy extends AuthGuard('local') {}
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+    getRequest(context: ExecutionContext): Request {
+        const ctx = GqlExecutionContext.create(context);
+        const { req } = ctx.getContext();
+        req.body = ctx.getArgs().loginUserInput;    
+        return req;
+    }
+}
