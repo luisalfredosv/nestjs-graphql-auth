c7222265e19fcd91dc5c64e90ce6c56f7f279e7b
lsarabia <lsarabia@DESKTOP-8QTCH9J.local>
2021-08-10 09:51:18 -0400

    Import RedisModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisClientModule } from './redis-client/redis-client.module';
+
+const logger = new Logger();
 
 @Module({
   imports: [
@@ -15,6 +20,33 @@ import { UserModule } from './user/user.module';
       autoSchemaFile: true,
       debug: true,
     }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        config: {
+          // host: configService.get<string>('REDIS_HOST'),
+          // port: configService.get<number>('REDIS_PORT'),
+          // username: configService.get<string>('REDIS_USERNAME'),
+          // password: configService.get<string>('REDIS_PASSWORD'),
+          // enableAutoPipelining: configService.get('REDIS_AUTO_PIPELINIG'),
+          namespace: configService.get('REDIS_NAMESPACE'),
+          // db: 2,
+          url: configService.get('REDIS_URL'),
+          onClientCreated(client) {
+            client.on('ready', () => {
+              logger.log('REDIS Ok')
+            });
+            client.on('error', err => {
+              logger.error('REDIS ERR')
+            });
+          },
+        }
+    }),
+    // useClass:
+    // useExisting:
+  }),
+    RedisClientModule
   ],
 })
 export class AppModule {}
